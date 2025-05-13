f001beacf424d33e343f92bfdec92abe11432aa9
luigiay1123 <wemixmir801@gmail.com>
2025-05-13 14:20:27 +0800

    style for empty list

--- book-tracker/public/index.js
+++ book-tracker/public/index.js
@@ -1,3 +1,4 @@
+  // Nav Sort
   document.addEventListener('DOMContentLoaded', () => {
     const sortToggle = document.getElementById('sortToggle');
     const sortForm = document.getElementById('sortForm');
@@ -21,4 +22,59 @@
       localStorage.removeItem('sortFormVisible');
     });
   });
+
+  // modal
+
+  
+
+  document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("confirmationModal");
+  const modalTitle = document.getElementById("modalTitle");
+  const modalMessage = document.getElementById("modalMessage");
+  const modalForm = document.getElementById("modalForm");
+  const cancelBtn = document.getElementById("cancelBtn");
+  const confirmBtn = document.getElementById("confirmBtn");
+
+    // Open modal with custom content
+    window.openModal = ({title = "Are you sure?", message, action, confirmLabel = "Confirm"}) => {
+      modalTitle.textContent = title;
+      modalMessage.innerHTML = message;
+      modalForm.setAttribute("action", action);
+      confirmBtn.innerHTML = confirmLabel;
+
+      modal.classList.add("show");
+      modal.classList.remove("hidden");
+    }
+
+  const closeModal = function () {
+    modal.classList.remove("show");
+    setTimeout(() => modal.classList.add("hidden"), 300);
+    modalForm.removeAttribute("action");
+  };
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) closeModal(); 
+  });
+
+  cancelBtn.addEventListener("click", closeModal);
+
+  document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape' && modal.classList.contains('show')){
+      closeModal();
+    }
+  });
+
+  //Trigger modal via data-* attributes
+  document.querySelectorAll(".open-modal-btn").forEach(button => {
+    button.addEventListener("click", () => {
+      const title = button.getAttribute("data-title");
+      const message = button.getAttribute("data-message");
+      const action = button.getAttribute("data-action");
+      const confirmLabel = button.getAttribute("data-label");
+
+      openModal({ title, message, action, confirmLabel });
+    });
+  });
+});
+
 
